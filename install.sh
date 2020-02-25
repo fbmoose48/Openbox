3aaae03e2df2830ec4cdaf6e4c473be1f3eb3665
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2020-02-25 21:27:03 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#Arch
+#sudo pacman -S openbox obconf tint2 nitrogen -y
+#Optional
+#sudo pacman -S xarchiver lxappearance pcmanfm
+
+#Ubuntu
+#sudo apt install openbox obconf tint2 nitrogen -y
+#Optional
+#sudo apt install xarchiver lxappearance pcmanfm
