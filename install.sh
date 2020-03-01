d805242c18c66ff87792dfa75d568fdd32798e4c
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2020-03-01 05:14:19 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
-#Arch
+##Arch
 #sudo pacman -S openbox obconf tint2 nitrogen arandr -yy
 #Optional
-#sudo pacman -S obmenu pamac-tray xarchiver lxappearance pcmanfm -yy
+#sudo pacman -S obmenu pamac-tray xarchiver pcmanfm -yy
+#yay -S obmenu-generator lxappearance lxappearance-obconf
+
 #Create Config Driectory
 #mkdir ~/.config/openbox
 #Creat autostart
 #nano ~/.config/openbo/autostart
+
 #Copy default configs
 #cp /etc/xdg/openbox/* ~/.config/openbox/
 
-#Ubuntu
+########################
+##Ubuntu
 #sudo apt install openbox obconf tint2 nitrogen -yy
 #Optional
 #sudo apt install xarchiver lxappearance pcmanfm -yy
