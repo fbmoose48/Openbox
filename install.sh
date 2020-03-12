610bb8f93f5e9b7b82188a0d6d95827b5b45544a
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2020-03-12 05:20:43 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,39 @@ mkdir ~/.config/openbox
 
 #Create autostart
 nano ~/.config/openbox/autostart
-echo 
+echo '## Openbox Autostart' >> ~/.config/openbox/autostart
+echo '' >> ~/.config/openbox/autostart
+echo '#compton -b --config ~/.config/compton.conf &' >> ~/.config/openbox/autostart
+echo '' >> ~/.config/openbox/autostart
+echo '# Launch Tint2' >> ~/.config/openbox/autostart
+echo '(sleep 0; tint2) &' >> ~/.config/openbox/autostart
+echo '' >> ~/.config/openbox/autostart
+echo '# Thunar starting in daemon mode' >> ~/.config/openbox/autostart
+echo '#thunar --daemon &' >> ~/.config/openbox/autostart
+echo '' >> ~/.config/openbox/autostart
+echo '# Bluetooth' >> ~/.config/openbox/autostart
+echo '# (sleep 2; blueberry-tray) &' >> ~/.config/openbox/autostart
+echo '' >> ~/.config/openbox/autostart
+echo '#Launch Nitrogen then Plank' >> ~/.config/openbox/autostart
+echo '(sleep 1; nitrogen --restore && plank) &' >> ~/.config/openbox/autostart
+echo '' >> ~/.config/openbox/autostart
+echo '# Numlock' >> ~/.config/openbox/autostart
+echo 'numlockx &' >> ~/.config/openbox/autostart
+echo '' >> ~/.config/openbox/autostart
+echo '# Volume control systray' >> ~/.config/openbox/autostart
+echo '(sleep 2; volumeicon) &' >> ~/.config/openbox/autostart
+echo '#(sleep2; pasytray) &' >> ~/.config/openbox/autostart
+echo '' >> ~/.config/openbox/autostart
+echo '# Enable power management' >> ~/.config/openbox/autostart
+echo '#xfce4-power-manager &' >> ~/.config/openbox/autostart
+echo '' >> ~/.config/openbox/autostart
+echo '# Clipboard Manager' >> ~/.config/openbox/autostart
+echo '#(sleep 2; clipit) &' >> ~/.config/openbox/autostart
+echo '' >> ~/.config/openbox/autostart
+echo '# XFCE4 Settings' >> ~/.config/openbox/autostart
+echo '#xfsettingsd &' >> ~/.config/openbox/autostart
+echo '' >> ~/.config/openbox/autostart
+echo '(sleep 5; nm-applet) &' >> ~/.config/openbox/autostart
 
 #Copy default configs
 cp /etc/xdg/openbox/* ~/.config/openbox/
