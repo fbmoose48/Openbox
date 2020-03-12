0a81d6f9d5cfbdc6ddb0eefdddd575438ab822bd
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2020-03-12 05:12:28 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 ##Arch
-#sudo pacman -S openbox obconf tint2 nitrogen arandr -yy
+sudo pacman -S openbox obconf tint2 nitrogen arandr -yy
+
 #Optional
-#sudo pacman -S obmenu compton plank pamac-tray xarchiver pcmanfm -yy
-#yay -S obmenu-generator lxappearance lxappearance-obconf
+sudo pacman -S obmenu compton plank pamac-tray xarchiver pcmanfm -yy
+yay -S obmenu-generator lxappearance lxappearance-obconf
 
 #Create Config Driectory
-#mkdir ~/.config/openbox
-#Creat autostart
-#nano ~/.config/openbox/autostart
+mkdir ~/.config/openbox
+
+#Create autostart
+nano ~/.config/openbox/autostart
+echo 
 
 #Copy default configs
-#cp /etc/xdg/openbox/* ~/.config/openbox/
+cp /etc/xdg/openbox/* ~/.config/openbox/
 
 ########################
 ##Ubuntu
